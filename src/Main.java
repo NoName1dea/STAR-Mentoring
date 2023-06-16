5fc50414a0632e32cf0166bb035cd5ef71dda54a
NoName1dea <113446267+NoName1dea@users.noreply.github.com>
2023-06-16 20:08:42 +0000

    Mofe varibales

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main{
         float numberofPeopleStalkingMe = 100.1F;
         double numberOfTimesIhadAStroke= 2.2;
         boolean myHappiness = false;
+        numberOfDogs = 0;
+        numberofPeopleStalkingMe = 0;
     }
 }
 //if broken=true, fix;

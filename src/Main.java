2284271843bb7456b55503b0a066e58cc5b78559
NoName1dea <113446267+NoName1dea@users.noreply.github.com>
2023-06-16 20:20:29 +0000

    time refremecnce time stuff

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
+import java.util.Date;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        int numberOfDogs = 1;
-        System.out.println(numberOfDogs);
-        byte numberOfPeopleInMyHouse= 3;
-        float numberofPeopleStalkingMe = 100.1F;
-        double numberOfTimesIhadAStroke= 2.2;
-        boolean myHappiness = false;
-        numberOfDogs = 0;
-        numberofPeopleStalkingMe = 0;
+        Date now = new Date();
+        System.out.println(now.getTime());
     }
 }
 //if broken=true, fix;

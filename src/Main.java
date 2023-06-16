1ceb830e1ee3b64a1d59572215f64452eb5435dc
NoName1dea <113446267+NoName1dea@users.noreply.github.com>
2023-06-16 19:50:24 +0000

    did stufff

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args) {
-    System.out.println("Hello World");
+        System.out.println("Hello World");
+        int numberOfDogs = 1;
+        double encoderPosition = 0;
     }
 }
+//if broken=true, fix;
